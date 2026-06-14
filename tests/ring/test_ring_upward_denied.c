b9c18fe719d2ad6f2c8c2e690d75066ee5a3739a
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-14 11:04:44 -0700

    tests: add ring upward denied test

---
+++ tests/ring/test_ring_upward_denied.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "atarix/ring.h"
+
+int main(void) {
+    int failures = 0;
+
+    if (atarix_ring_can_access(ATARIX_RING_APPLICATION,
+                               ATARIX_RING_SUPERVISOR,
+                               ATARIX_RING_OPERATION_REQUEST) != ATARIX_RING_STATUS_DENY) {
+        printf("application to supervisor should be denied\n");
+        failures++;
+    }
+
+    if (atarix_ring_can_access(ATARIX_RING_DEVICE,
+                               ATARIX_RING_FABRIC,
+                               ATARIX_RING_OPERATION_REQUEST) != ATARIX_RING_STATUS_DENY) {
+        printf("device to fabric should be denied\n");
+        failures++;
+    }
+
+    return failures ? 1 : 0;
+}
