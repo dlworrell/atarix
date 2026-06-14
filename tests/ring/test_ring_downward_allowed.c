ca27c9090c0677c2d2b5455a6177763d00a28535
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-14 11:17:28 -0700

    tests: align ring downward test with shared rings

--- tests/ring/test_ring_downward_allowed.c
+++ tests/ring/test_ring_downward_allowed.c
@@ -6,16 +6,16 @@ int main(void) {
     int failures = 0;
 
     if (atarix_ring_can_access(ATARIX_RING_SUPERVISOR,
-                               ATARIX_RING_DEVICE,
+                               ATARIX_RING_DRIVER,
                                ATARIX_RING_OPERATION_MANAGE) != ATARIX_RING_STATUS_ALLOW) {
-        printf("supervisor manage device should be allowed\n");
+        printf("supervisor manage driver should be allowed\n");
         failures++;
     }
 
     if (atarix_ring_can_access(ATARIX_RING_FABRIC,
-                               ATARIX_RING_DEVICE,
+                               ATARIX_RING_DRIVER,
                                ATARIX_RING_OPERATION_REQUEST) != ATARIX_RING_STATUS_ALLOW) {
-        printf("fabric request to device should be allowed\n");
+        printf("fabric request to driver should be allowed\n");
         failures++;
     }
 
